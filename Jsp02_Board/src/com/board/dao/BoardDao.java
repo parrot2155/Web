bfcebb01439e4f55ea6ce671518adcd3a3ac04c8
Park Tae Wook <parrot2155@gmail.com>
2025-06-19 09:51:38 +0900

    Jsp02_Board 시작

---
+++ Jsp02_Board/src/com/board/dao/BoardDao.java
@@ -0,0 +1,35 @@
+package com.board.dao;
+
+import java.util.List;
+
+import com.board.dto.BoardDto;
+
+public class BoardDao {
+	//전체 조회
+	public List<BoardDto> selectAll(){
+		return null;
+		
+	}
+	
+	//선택 조회
+	public BoardDto selectOne(int seq) {
+		return null;
+	}
+	
+	//글 추가
+	public int insert(BoardDto dto) {
+		return 0;
+		
+	}
+	
+	//글 수정
+	public int update(BoardDto dto) {
+		return 0;
+		
+	}
+	//글 삭제
+	public int delete(int seq) {
+		return 0;
+	
+	}
+}
